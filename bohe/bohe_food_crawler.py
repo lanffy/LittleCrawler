25a4f6538e26b027b8d5a0043c4d02c3af1dc25c
lanffy <rao695901563@gmail.com>
2017-11-13 16:36:38 +0800

    accomplished crawler

--- bohe/bohe_food_crawler.py
+++ bohe/bohe_food_crawler.py
@@ -10,6 +10,7 @@ import math
 import time
 reload(sys)
 sys.setdefaultencoding('utf8')
+import db
 
 #proxies = shield_proxy.getListProxies()
 #print proxies
@@ -39,32 +40,51 @@ def handPageDetail(page_url):
     classify_string = re.sub('\s','',classify_element_a[1].string)
     classify_h2_text = re.sub('\s','',classify_element_h2.text)
     index = classify_h2_text.rfind('/')
-    name = classify_h2_text[index+1:]
+    fname = classify_h2_text[index+1:]
     img = page_detail.find('div',attrs={'class':'food-pic pull-left'}).find('img')['src']
     alias_div = page_detail.find('ul',attrs={'class':'basic-infor'})
     alias_div_li = alias_div.find('li').text
     alias_div_li_name = re.sub(ur'别名：','',alias_div_li)
-    alias = '无'
+    alias = ''
     if(len(alias_div_li) != len(alias_div_li_name)):
         alias = alias_div_li_name
-    print 'classify:' + classify_string + ';name:' + name + ';alias:' + alias + ';image:' + img
+    print 'classify:' + classify_string + ';name:' + fname + ';alias:' + alias + ';image:' + img
     nutrition_info = page_detail.find('div',attrs={'class':'nutr-tag margin10'})
     nutrition_info2 = nutrition_info.find_all('dd',attrs={})
     nutrition_info3 = nutrition_info2[2:6]
     print '营养信息:'
+    heat=carbohydrate=fat=protein=0
     for info in nutrition_info3:
         name = info.find('span', attrs={'class':'dt'}).string
         value = info.find('span', attrs={'class':'dd'}).string
+        if(name == '热量(大卡)'):
+            heat = value
+        if(name == '碳水化合物(克)'):
+            carbohydrate = value
+        if(name == '脂肪(克)'):
+            fat = value
+        if(name == '蛋白质(克)'):
+            protein = value
         print name + ':' + value
+    print heat
+    print carbohydrate
+    print fat
+    print protein
+    classify_id = db.insertFoodClassify(classify_string)
+    food_id = db.insertFood(fname, alias,heat,carbohydrate,fat,protein,classify_id,img)
     more_nutrition_info = page_detail.find('div',attrs={'class':'widget-unit'})
-    more_nutrition_info2 = more_nutrition_info.find('tbody')
-    tr_list = more_nutrition_info2.find_all('tr')
-    print '度量单位(大卡):'
-    for tr in tr_list:
-        tds = tr.find_all('td')
-        name = tds[0]
-        value = tds[1]
-        print getMoreNutritionInfo(name) + ':' + re.sub(ur'大卡','',getMoreNutritionInfo(value))
+    if(more_nutrition_info):
+        more_nutrition_info2 = more_nutrition_info.find('tbody')
+        tr_list = more_nutrition_info2.find_all('tr')
+        print '度量单位(大卡):'
+        for tr in tr_list:
+            tds = tr.find_all('td')
+            name = tds[0]
+            value = tds[1]
+            food_classify_name = getMoreNutritionInfo(name)
+            heat = re.sub(ur'大卡','',getMoreNutritionInfo(value))
+            print food_classify_name + ':' + heat
+            db.insertFoodEnergyRuler(food_id,food_classify_name,heat)
     print '==333==  end  hand detail page'
 
 def getMoreNutritionInfo(elment):
@@ -93,23 +113,25 @@ def handListPage(uri_list):
         page_detail_url = host + page_uri
         handPageDetail(page_detail_url)
 
-#handPageDetail('http://www.boohee.com/shiwu/yumi_xian')
+#handPageDetail('http://www.boohee.com/shiwu/fddce61d')
+#exit(0)
 #getListPages('http://www.boohee.com/food/group/1?page=1')
 
-def listPageHandler(group_url):
+def listPageHandler(group_index,group_url):
+    print '==G'+str(group_index)+'== start hand group:' + group_url
     for page in range(1, max_page+1):
         page_url = group_url + '?page=' + str(page)
         page_list = getListPages(page_url)
         print '=='+str(page)+'== strat hand list page:' + page_url
         handListPage(page_list)
         print '=='+str(page)+'==  end  hand list page'
+    print '==G'+str(group_index)+'==  end  hand group'
 
 def main():
     for group_index in range(1, max_group+1):
         group_url = food_classify_url + str(group_index)
-        print '==G'+str(group_index)+'== start hand group:' + group_url
-        listPageHandler(group_url)
-        print '==G'+str(group_index)+'==  end  hand group'
+        listPageHandler(group_index,group_url)
+    listPageHandler('last','http://www.boohee.com/food/view_menu')
 
 
 max_group = 10
---
+++ bohe/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+#encoding:utf-8
+import sys
+import re
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import MySQLdb
+
+#create a cursor
+
+def insertFoodClassify(classify_name):
+    conn = MySQLdb.connect('localhost','root','RaoLiang','test')
+    print 'insertFoodClassify:' + classify_name
+    classify_name = re.sub(ur'\'|\"','',classify_name)
+    cursor = conn.cursor()
+    cursor.execute("select id from food_classify where classify_name='"+classify_name+"'")
+    result = cursor.fetchall()
+    if(result):
+        return result[0][0]
+    cursor.execute("INSERT INTO food_classify(classify_name) VALUES('"+classify_name+"')")
+    food_classify_id = int(cursor.lastrowid)
+    conn.commit()
+    conn.close()
+    return food_classify_id
+
+def insertFood(name,alias,heat,carbohydrate,fat,protein,classify_id,img):
+    conn = MySQLdb.connect('localhost','root','RaoLiang','test')
+    name = re.sub(ur'\'|\"','',name)
+    alias = re.sub(ur'\'|\"','',alias)
+    print 'insertFood:' + name+","+str(alias)+","+str(heat)+","+str(carbohydrate)+","+str(fat)+","+str(protein)+","+str(classify_id)+",'"+img
+    cursor = conn.cursor()
+    sql = "INSERT INTO food(name,alias,heat,carbohydrate,fat,protein,classify_id,img) VALUES('"+name+"','"+str(alias)+"',"+str(heat)+","+str(carbohydrate)+","+str(fat)+","+str(protein)+","+str(classify_id)+",'"+img+"')"
+    cursor.execute(sql)
+    food_id = int(cursor.lastrowid)
+    conn.commit()
+    conn.close()
+    return food_id
+
+def insertFoodEnergyRuler(food_id,food_classify_name,heat):
+    conn = MySQLdb.connect('localhost','root','RaoLiang','test')
+    food_classify_name = re.sub(ur'\'|\"','',food_classify_name)
+    print 'insertFoodEnergyRuler:' + str(food_id)+",'"+food_classify_name+"',"+str(heat)
+    cursor = conn.cursor()
+    sql = "INSERT INTO food_energy_ruler(food_id,food_classify_name,heat) VALUES("+str(food_id)+",'"+food_classify_name+"',"+str(heat)+")"
+    cursor.execute(sql)
+    food_energy_id = int(cursor.lastrowid)
+    conn.commit()
+    conn.close()
+    return food_energy_id
+
+print insertFoodClassify('te\'a\"st4')
+#print insertFood('玉米','别名',1.1,2.2,3.3,4.4,1,'http://www.baidu.com')
+#print insertFoodEnergyRuler(1,'玉米',1.22)
