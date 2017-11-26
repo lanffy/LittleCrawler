31b1f41028b73e5dbe53ff67422157478d4ff833
lanffy <rao695901563@gmail.com>
2017-11-26 16:40:56 +0800

    export food data

--- bohe/db.py
+++ bohe/db.py
@@ -48,6 +48,54 @@ def insertFoodEnergyRuler(food_id,food_classify_name,heat):
     conn.close()
     return food_energy_id
 
+allNames = []
+allAliases = []
+def exportData(startId):
+    conn = MySQLdb.connect('localhost','root','RaoLiang','test')
+    cursor = conn.cursor()
+    sql = "select * from food where id>" + str(startId) + " order by id asc limit 500 "
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    i = 0
+    if(len(results) == 0):
+        print "no more data"
+        exit(0)
+    for row in results:
+        i = row[0]
+        name = filter(row[1], False)
+        alias = []
+        if(row[2]):
+            alias = filter(row[2], True)
+        if(name):
+            if name not in allNames:
+                print name
+                allNames.append(name)
+                if(alias):
+                    for a in alias:
+                        if a not in allAliases:
+                            print "#" + a
+                            allAliases.append(a)
+    exportData(i)
+
+
+def filter(strIn,isArr):
+    if(strIn):
+        strIn = re.sub(ur'[，|，]','、',strIn.decode("utf8"))
+        strIn = re.sub(ur'(\(.*?\))','',strIn)
+        strIn = re.sub(ur'(（.*?\）)','',strIn)
+        if(not isArr):
+            return strIn
+        if(strIn):
+            strArr = strIn.split('、')
+            strArr = set(strArr)
+            return strArr
+        else:
+            return []
+    else:
+        return []
+
+exportData(0)
+#a = filter("a，(a)b(b)c(c)    a（a）b（b）c（c）",False)
 #print insertFoodClassify('te\'a\"st4')
 #print insertFood('玉米','别名',1.1,2.2,3.3,4.4,1,'http://www.baidu.com')
 #print insertFoodEnergyRuler(1,'玉米',1.22)
